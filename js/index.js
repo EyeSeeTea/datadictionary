5dd62463752563bcef7225460eddf04498db8d91
Arnau Sanchez <pyarnau@gmail.com>
2017-03-15 08:55:59 +0100

    Add string renderer to all de/ind fields for robustness

--- js/index.js
+++ js/index.js
@@ -1244,15 +1244,18 @@ function DataManager() {
 			},
 			{
 				data : 'aggregationType',
-				"title" : "Aggregation Type"
+				"title" : "Aggregation Type",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'dataElementGroups[,].name',
-				"title" : "DE Groups"
+				"title" : "DE Groups",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'user.displayName',
-				"title" : "Created by"
+				"title" : "Created by",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'created',
@@ -1357,7 +1360,7 @@ function DataManager() {
 			});
 		} else {
 			oTable.fnClearTable();
-			if (dataList.length > 0){
+			if (dataList.length > 0) {
 				oTable.fnAddData(dataList);
 				oTable.fnAdjustColumnSizing();
 			}
@@ -1431,7 +1434,8 @@ function DataManager() {
 			},
 			{
 				data : 'displayShortName',
-				"title" : "Short Name"
+				"title" : "Short Name",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'id',
@@ -1443,19 +1447,23 @@ function DataManager() {
 			},
 			{
 				data : 'numeratorDescription',
-				"title" : "Numerator Description"
+				"title" : "Numerator Description",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'denominatorDescription',
-				"title" : "Denominator Description"
+				"title" : "Denominator Description",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'numerator',
-				"title" : "Numerator"
+				"title" : "Numerator",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'denominator',
-				"title" : "Denominator"
+				"title" : "Denominator",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'annualized',
@@ -1464,11 +1472,13 @@ function DataManager() {
 			},
 			{
 				data : 'indicatorType.name',
-				"title" : "Indicator Type"
+				"title" : "Indicator Type",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'dataSets[,].name',
-				"title" : "DataSets"
+				"title" : "DataSets",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'description',
@@ -1481,7 +1491,8 @@ function DataManager() {
 			},
 			{
 				data : 'user.displayName',
-				"title" : "Created by"
+				"title" : "Created by",
+				"render" : Util.getDataTableRenderer("string")
 			},
 			{
 				data : 'created',
